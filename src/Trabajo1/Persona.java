dfaac9fcc42e384d52eab9222a1da3e1b684b3fb
LeonardoEchegaray15 <105308866+LeonardoEchegaray15@users.noreply.github.com>
2023-09-08 13:14:40 -0500

    Add files via upload

---
+++ src/Trabajo1/Persona.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Trabajo1;
+
+/**
+ *
+ * @author ESTUDIANTE
+ */
+public class Persona {
+    private String Nombre;
+
+
+    public Persona(String Nombre) {
+        this.Nombre = Nombre;
+
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+
+}
